01b5bf842b624ea33e3d762e37d1a3ee6374797e
Kopyander <jkopecnak@gmail.com>
2022-12-10 17:01:57 +0100

    cele znovu

--- ukol_2.py
+++ ukol_2.py
@@ -1,101 +1,83 @@
-import csv 
+import csv
 
-
-
-with open('try_data.csv','r' ) as csv_file, open("vystup_7denni.csv", "w", encoding="utf-8", newline="") as csv_out_week,open("vystup_rok.csv", "w", encoding="utf-8", newline="") as csv_out_year:
+with open('data.csv','r' ) as csv_file, open("vystup_test.csv", "w", encoding="utf-8", newline="") as csv_test_1, open("vystup_test_year.csv", "w", encoding="utf-8", newline="") as csv_test_year:
     csv_reader=csv.reader(csv_file) # takhle to vytiskne jako list, ve kterim mužeš makat jako frajer 
 
-    writer_week = csv.writer(csv_out_week)
-    writer_year = csv.writer(csv_out_year)
+    writer_week = csv.writer(csv_test_1)
+    writer_year = csv.writer(csv_test_year)
     
-    #Sumy prutků 
-    total=0
+    prefix=None #data ve všech řádcích stejná 
+    first_day=None #první den týdne
+    first_day_year=None #první den roku 
+    counter_week= 0 #počítá dny v týdnu 
+    counter_year=0 #počíta dny v roce 
+    sum_week=0 
     sum_year=0
-    # počítaní dnů 
-    counter=0
-    year_counter=0
-    rok=0
-    first_day=None
-    first_day_year=None
-    prefix=None
-    
+    year_base=0
+    month_year=None
+    chyby_v_prutoku=[]
     
     for line in csv_reader:
-        print(counter)###
         
-        if prefix is None:  #prefix zustava 
+        datum=line[2]
+        year=[int(datum.split('.')[2])]
+        
+        if prefix is None: 
             prefix=line[0:2]
-            
+            print(prefix)
         
-        if rok ==0:
-            datum=line[2]
-            rok=[int(datum.split('.')[2])]
-            vystup_rok_iter=int(datum.split('.')[2])
-            print(vystup_rok_iter)
-            
         if first_day is None:
             datum=line[2]
             first_day=[int(datum.split('.')[0])]   
             month=[int(datum.split('.')[1])]
+            
         
         if first_day_year is None:
             datum=line[2]
-            first_day_year=[int(datum.split('.')[0])]
-            
-        # vypočet roku iterarace
-        datum=line[2]
-        vystup_rok_iter=[int(datum.split('.')[2])]
-        
-            
-        if vystup_rok_iter != rok:
-            rocni_flow=(sum_year/year_counter)
-            print(rocni_flow)
-            writer_week.writerow(prefix + first_day_year + month + [round(sum_year/year_counter, 4)])
-            year_counter=0 
-            datum=line[2]
             first_day_year=[int(datum.split('.')[0])] 
-            rok=[int(datum.split('.')[2])]
-            print("změna roku iterace") #koment 
             
-        # týdení count 
-        if counter==7:
-            prumer=(total/counter)
-            print(prumer)
-            writer_week.writerow(prefix + first_day + month + [round(total/counter, 4)])
-            counter=0
-            total=0
-            
+        if year_base==0:
             datum=line[2]
-            first_day=int(datum.split('.')[0])   
-            month=int(datum.split('.')[1])
+            year_base= [int(datum.split('.')[2])]
+        
+        if month_year is None:
+            datum=line[2]
+            month_year=[int(datum.split('.')[1])]
             
+        #nekorektní vstupy 
+        if float(line[3]) <= 0:
+            chyba_den=[int(datum.split('.')[0])] 
+            chyba_month=[int(datum.split('.')[1])]
+            chyba_year=[int(datum.split('.')[2])]
             
-            
-        counter+=1
-        year_counter+=1
-        total+=float(line[3])
-        sum_year+=float(line[3])
-
+            chyby_v_prutoku.append(chyba_den+chyba_month+chyba_year)
         
         
-        #prutok=line(float[3])
-    #print(total)
-    #print(counter)
-    #prumer=(total/counter)
-    #print(prumer)
-
+        if year_base!=year:
+            writer_year.writerow(prefix + first_day_year+ month_year + year_base+ [round(sum_year/counter_year, 4)])
+            counter_year=0
+            sum_year=0
+            #datum=line[2]
+            month_year=[int(datum.split('.')[1])]
+            year_base= [int(datum.split('.')[2])]
+        
+        
+        if counter_week==7:
+            writer_week.writerow(prefix + first_day+ month+year+ [round(sum_week/counter_week, 4)])
+            counter_week=0
+            sum_week=0
+            # Nové datum 
+            datum=line[2]
+            first_day=[int(datum.split('.')[0])]   
+            month=[int(datum.split('.')[1])]
+            year= [int(datum.split('.')[2])]
+            
     
+        counter_week+=1
+        sum_week+=float(line[3])
+        counter_year+=1
+        sum_year+=float(line[3]) 
         
-        
-    
-        
-            
-
-        
-        
-
-
-
-
-
-    
+        #cyklus skončí před dokončením roku, nebo týdne 
+    writer_year.writerow(prefix + first_day_year+ month_year + year_base+ [round(sum_year/counter_year, 4)])
+    writer_week.writerow(prefix + first_day+ month+year+ [round(sum_week/counter_week, 4)])
